06a93a69806053548b9e0c3d12491b89baf6828a
haris khan <haris.khan87@gmail.com>
2024-12-09 15:26:04 +0100

    add newrelic

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -32,6 +32,7 @@ export default function Index() {
           <p className="leading-6 text-gray-700 dark:text-gray-200">
             What&apos;s next?
           </p>
+          <a href="/contact">Contact</a>
           <ul>
             {resources.map(({ href, text, icon }) => (
               <li key={href}>
